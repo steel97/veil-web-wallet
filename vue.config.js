0f2417ed4737c79d77050efdd288ca4087efb4d4
Ivan Yv <steel-97@mail.ru>
2022-09-27 00:44:04 +0300

    App version

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 const { defineConfig } = require("@vue/cli-service");
 const webpack = require("webpack");
+const childProcess = require("child_process"); let lastCommitHash = ""; try {
+  lastCommitHash = childProcess
+    .execSync("git rev-parse --short HEAD")
+    .toString()
+    .trim();
+} catch (e) {
+  console.error(e);
+}
 
 module.exports = defineConfig({
   transpileDependencies: true,
@@ -17,6 +25,14 @@ module.exports = defineConfig({
     config.plugins.push(...[new webpack.ProvidePlugin({
       Buffer: ["buffer", "Buffer"],
     })]);
+
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        "process.env": {
+          VUE_APP_COMMIT_HASH: JSON.stringify(lastCommitHash),
+        },
+      }),
+    );
 
     config.resolve.fallback = {
       "stream": require.resolve("stream-browserify"),
