389906886030f232057a3749678e380e4f8a3462
Ivan Yv <steel-97@mail.ru>
2022-09-27 08:57:00 +0300

    Fixed table layout

--- src/components/TransactionsTable.vue
+++ src/components/TransactionsTable.vue
@@ -3,7 +3,7 @@
         <a v-for="utx, index in utxos" :key="utx.txid + '-'+index"
             class="p-2 border-b-2 border-gray-400 w-full text-sm flex justify-between items-center"
             :href="LightwalletService.txViewUrl + utx.txid" target="_blank">
-            <div class="flex items-center">
+            <div>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 mr-2"
                     v-if="!utx.pending">
                     <path fill-rule="evenodd"
@@ -16,6 +16,8 @@
                         d="M4.755 10.059a7.5 7.5 0 0112.548-3.364l1.903 1.903h-3.183a.75.75 0 100 1.5h4.992a.75.75 0 00.75-.75V4.356a.75.75 0 00-1.5 0v3.18l-1.9-1.9A9 9 0 003.306 9.67a.75.75 0 101.45.388zm15.408 3.352a.75.75 0 00-.919.53 7.5 7.5 0 01-12.548 3.364l-1.902-1.903h3.183a.75.75 0 000-1.5H2.984a.75.75 0 00-.75.75v4.992a.75.75 0 001.5 0v-3.18l1.9 1.9a9 9 0 0015.059-4.035.75.75 0 00-.53-.918z"
                         clip-rule="evenodd" />
                 </svg>
+            </div>
+            <div class="flex items-center overflow-hidden truncate">
 
                 <div class="overflow-hidden truncate text-center fixed-tx-w">
                     {{utx.txid}}
